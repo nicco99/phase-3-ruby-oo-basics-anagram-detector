f259d1d26526d8ad7d72492f844d91ff02a77e61
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-10-27 10:17:23 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+
+
+def match arr 
+    arr.select do |word|
+@word.chars.sort == word.chars.sort
+    end
+end
+
+def initialize word
+    @word = word
+end
+end
